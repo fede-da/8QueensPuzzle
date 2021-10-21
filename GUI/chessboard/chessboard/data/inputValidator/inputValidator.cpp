76895efab56ecf6c19bbeda522263e91227cfc0d
Federico D'Armini <63853572+fede-da@users.noreply.github.com>
2021-10-21 03:30:04 +0200

    Add files via upload

---
+++ GUI/chessboard/chessboard/data/inputValidator/inputValidator.cpp
@@ -0,0 +1,8 @@
+//
+//  inputValidator.cpp
+//  8QueensPuzzle
+//
+//  Created by Federico D'Armini on 15/10/21.
+//
+
+#include "inputValidator.hpp"
---
+++ GUI/chessboard/chessboard/data/inputValidator/inputValidator.hpp
@@ -0,0 +1,18 @@
+//
+//  inputValidator.hpp
+//  8QueensPuzzle
+//
+//  Created by Federico D'Armini on 15/10/21.
+//
+
+#ifndef inputValidator_hpp
+#define inputValidator_hpp
+
+class InputValidator{
+    int row;
+    int col;
+public:
+    void readInput();
+};
+
+#endif /* inputValidator_hpp */

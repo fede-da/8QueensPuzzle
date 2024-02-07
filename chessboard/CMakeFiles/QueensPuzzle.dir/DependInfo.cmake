2c14b4b654f4ced6dcc0abbaa23e9fa3bac733ea
kaguyasama <fede97d@hotmail.it>
2024-02-07 18:07:46 +0100

    Makefile added

---
+++ chessboard/CMakeFiles/QueensPuzzle.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kaguyasama/GitHub/8QueensPuzzle/chessboard/ResourcePath.mm" "CMakeFiles/QueensPuzzle.dir/ResourcePath.mm.o" "gcc" "CMakeFiles/QueensPuzzle.dir/ResourcePath.mm.o.d"
+  "/Users/kaguyasama/GitHub/8QueensPuzzle/chessboard/algorithms/Puzzle.cpp" "CMakeFiles/QueensPuzzle.dir/algorithms/Puzzle.cpp.o" "gcc" "CMakeFiles/QueensPuzzle.dir/algorithms/Puzzle.cpp.o.d"
+  "/Users/kaguyasama/GitHub/8QueensPuzzle/chessboard/algorithms/Queen.cpp" "CMakeFiles/QueensPuzzle.dir/algorithms/Queen.cpp.o" "gcc" "CMakeFiles/QueensPuzzle.dir/algorithms/Queen.cpp.o.d"
+  "/Users/kaguyasama/GitHub/8QueensPuzzle/chessboard/algorithms/chessboard.cpp" "CMakeFiles/QueensPuzzle.dir/algorithms/chessboard.cpp.o" "gcc" "CMakeFiles/QueensPuzzle.dir/algorithms/chessboard.cpp.o.d"
+  "/Users/kaguyasama/GitHub/8QueensPuzzle/chessboard/algorithms/square.cpp" "CMakeFiles/QueensPuzzle.dir/algorithms/square.cpp.o" "gcc" "CMakeFiles/QueensPuzzle.dir/algorithms/square.cpp.o.d"
+  "/Users/kaguyasama/GitHub/8QueensPuzzle/chessboard/main.cpp" "CMakeFiles/QueensPuzzle.dir/main.cpp.o" "gcc" "CMakeFiles/QueensPuzzle.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ chessboard/CMakeFiles/QueensPuzzle.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QueensPuzzle.dir/ResourcePath.mm.o"
+  "CMakeFiles/QueensPuzzle.dir/ResourcePath.mm.o.d"
+  "CMakeFiles/QueensPuzzle.dir/algorithms/Puzzle.cpp.o"
+  "CMakeFiles/QueensPuzzle.dir/algorithms/Puzzle.cpp.o.d"
+  "CMakeFiles/QueensPuzzle.dir/algorithms/Queen.cpp.o"
+  "CMakeFiles/QueensPuzzle.dir/algorithms/Queen.cpp.o.d"
+  "CMakeFiles/QueensPuzzle.dir/algorithms/chessboard.cpp.o"
+  "CMakeFiles/QueensPuzzle.dir/algorithms/chessboard.cpp.o.d"
+  "CMakeFiles/QueensPuzzle.dir/algorithms/square.cpp.o"
+  "CMakeFiles/QueensPuzzle.dir/algorithms/square.cpp.o.d"
+  "CMakeFiles/QueensPuzzle.dir/main.cpp.o"
+  "CMakeFiles/QueensPuzzle.dir/main.cpp.o.d"
+  "QueensPuzzle"
+  "QueensPuzzle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QueensPuzzle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
